70c28afc41779e905e52c23a310c948c28f87e8e
Ashutosh Singh <dh_ashu@outlook.com>
2013-10-08 19:02:53 +0530

    Started Learning python the hard way

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# Simple print
+print "I will now count my chickens:"
+
+# show in same line as hens
+print "Hens", 25+30/6
+#show in same line as Roosters
+print "Roosters", 100-25 * 3 % 5
+# directly print the calculation
+print 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 +6
+
+# no calculation inside double quotes
+print "Is is ture that 3+2 <5 -7 ?"
+
+#direct boolean calculation 
+print 3 + 2  < 5 -7
+
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh that's why it's false"
+
+#other operators
+print "How about some more."
+
+print "Is it greater", 5 > -2
+print "Is it greater or equal ?", 5 >= -2
+print "Is it less or equal ?" ,5 <=-2
